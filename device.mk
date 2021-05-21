433f783b4082ad85a7d96bf7724fd85ece590e47
LuK1337 <priv.luk@gmail.com>
2021-05-21 15:32:47 +0200

    thor: overlay: Set default Settings.Global.DEVICE_NAME according to MIUI
    MIUI uses ro.product.vendor.marketname for default device name.
    
    Change-Id: I310c1ffc581b72653783d1093d9e7ebde544bb16

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/thor/thor-vendor.mk)
 
+# Overlay
+PRODUCT_PACKAGES += \
+    SettingsProviderResThor
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
