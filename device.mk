efedfab0f2a3d932ba80617ce391efff8e9c1756
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-05 01:14:47 +0300

    thor: overlay: Allow switching between 60Hz and 120Hz
    Change-Id: I4f328dbdce50e709f30b1f098e71248d72819268

--- device.mk
+++ device.mk
@@ -14,6 +14,7 @@ $(call inherit-product, vendor/xiaomi/thor/thor-vendor.mk)
 PRODUCT_PACKAGES += \
     FrameworksResThor \
     SettingsProviderResThor \
+    SettingsResThor \
     SystemUIResThor \
     WifiResThor
 
