b4b5c352ab1d14a10b10b08b132fb25f11f558e9
Jens Reidel <adrian@travitia.xyz>
2023-05-11 17:53:50 +0200

    thor: Initial device tree
    Change-Id: I6045cc6dca40c7c4a788f43102207c6a71e60483
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

---
+++ lineage_thor.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from thor device
+$(call inherit-product, device/xiaomi/thor/device.mk)
+
+# Inherit from common lineage configuration
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_thor
+PRODUCT_DEVICE := thor
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := 2203121C
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
