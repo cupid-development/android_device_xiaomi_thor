c4bf5b694fe7ff8cf0a6594db57590554a040a84
Jens Reidel <adrian@travitia.xyz>
2023-05-11 17:53:50 +0200

    thor: Initial device tree
    Change-Id: I6045cc6dca40c7c4a788f43102207c6a71e60483
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_thor.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_thor-user \
+    lineage_thor-userdebug \
+    lineage_thor-eng
